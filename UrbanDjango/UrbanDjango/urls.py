e231c7d9401e158cc5abbd405f8b4bab1a4bc8a9
evgen81Git <evgeniynrtv@gmail.com>
2024-09-10 02:16:52 +0700

    задание 18_2 Добавлено приложение task2, папка templates с шаблонами для классового и функционального представления;

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import *
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', class_templ.as_view(), name='class_templ'),
+    path('func_templ', func_templ)
 ]
--- UrbanDjango/task2/views.py
+++ UrbanDjango/task2/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class class_templ(TemplateView):
+    template_name = 'class_template.html'
+
+def func_templ(request):
+    return render(request, 'func_template.html')
